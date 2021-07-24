42fe0f5dca84a300a8fd08a09beaff16a78c5fd6
Issa Jean Marie <jeanmarieissa@gmail.com>
2021-07-24 16:05:06 +0200

    Admin restrictions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>How to fail changes</p>
+				<p>Now me can't merge it either</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
